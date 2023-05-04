8027fda169af907e93c0edbee079a040805a7745
Estrallator <manubazlo@gmail.com>
2023-05-04 12:28:30 +0200

    First functional version. Console mode. Only one gamemode

--- dataParser.java
+++ dataParser.java
@@ -1,24 +1,41 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class dataParser {
 
-    private ArrayList<ArrayList<String>> phrasalVerbs = new ArrayList<>();
+    //phrasal verbs array. Contains data in 4 indexes 0-verb 1-translation 2-example 3-translated example
+    public ArrayList<ArrayList<String>> phrasalVerbs = new ArrayList<>(4);
+
+    //Parser constructor initializes the 4 dimensions of phrasalVerbs array, parses and stores adds to array the data contained in data.txt
     public dataParser(){
+
+        String[] dataArr;
+
+        for(int i=0; i <=3; i++) {
+            phrasalVerbs.add(new ArrayList());
+        }
 
         try {
             File myObj = new File("data.txt");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.println(data);
+                dataArr=data.split("/");
+                //If the line parsed does not contain 4 indexes, data is wrong so we skip line
+                if(dataArr.length == 4){
+                    for(int n=0; n<=3 ; n++){
+                        phrasalVerbs.get(n).add(dataArr[n]);
+                    }
+                }
             }
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+
     }
 
     @Override
@@ -38,5 +55,19 @@ public class dataParser {
             e.printStackTrace();
         }
         return out;
+    }
+
+    //Returns a random phrasal verb data array
+    public String[] getRandom(){
+        int index = 0;
+        int length = phrasalVerbs.get(0).size();
+        String[] verbData = new String[4];
+        index = (int)(Math.random()*length);
+
+        for(int i=0 ; i<=3 ; i++){
+            verbData[i] = phrasalVerbs.get(i).get(index);
+        }
+
+        return verbData;
     }
 }
--- mainClass.java
+++ mainClass.java
@@ -1,11 +1,81 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 public class mainClass {
 
+    public static final String version ="0.1b";
+    public static final String author ="Manuel B.";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+
     public static void main(String[] args){
+        System.out.println("PHRASAL VERBS TRAINER version: " + version + " coded by: " + author + "\n\n");
         dataParser phrasalsObj = new dataParser();
-        System.out.println(phrasalsObj);
+        mainClass app = new mainClass();
+        app.selectTheRight(phrasalsObj);
+    }
+
+    //Game mode. Prints a incomplete phrase and select the right verb between several options
+    public void selectTheRight(dataParser phrasals){
+        System.out.println("GAMEMODE : WRITE THE RIGHT OPTION\n");
+        while(true) {
+            String[] right = phrasals.getRandom();
+            String[] options = {right[0], phrasals.getRandom()[0], phrasals.getRandom()[0], phrasals.getRandom()[0], phrasals.getRandom()[0]};
+            int[] indexes = this.shuffleIndexes(options.length);
+            //System.out.println("answer: " + right[0]);
+            System.out.println(ANSI_BLUE + right[2].toLowerCase().replace(right[0].trim().toLowerCase(), " ____ ") + " / " + ANSI_YELLOW + right[3]+ ANSI_RESET);
+            System.out.print("OPCIONES:  "+ANSI_PURPLE);
+
+            for (int i = 0; i < indexes.length; i++) {
+                System.out.print(options[indexes[i]] + "  ");
+            }
+            System.out.print(ANSI_RESET + "\n");
+            System.out.print("Respuesta: ");
+            Scanner scanObj = new Scanner(System.in);
+            String userAnswer = scanObj.nextLine();
+
+            if (userAnswer.toLowerCase().trim().equals(right[0].toLowerCase().trim())) {
+                System.out.println(ANSI_GREEN +"CORRECTO!! " + right[0] + ANSI_RESET + right[1] + " : " + right[2] + " / " + right[3]);
+            } else {
+                System.out.println(ANSI_RED + "Incorrecto, la respuesta es: " + ANSI_RESET + right[0]);
+                System.out.println(right[0] + "-" + right[1] + " : " + right[2] + " / " + right[3]);
+            }
+            System.out.println("\n");
+        }
+    }
+
+    //Helper to get randomized indexes
+    public int[] shuffleIndexes(int n) {
+
+        Integer[] intArray = new Integer[n];
+
+        for(int i=0 ; i < n ; i++){
+            intArray[i] = i;
+        }
+
+        List<Integer> intList = Arrays.asList(intArray);
+        Collections.shuffle(intList);
+        Object[] indexes = intList.toArray();
+        int[] retArr = new int[indexes.length];
+
+        for (int i=0 ; i < indexes.length ; i++){
+            retArr[i] = (int)(indexes[i]);
+        }
+
+        return retArr;
     }
 
 }
